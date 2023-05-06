abe0e4219ca2790aae9b046f549e518d551002f4
Daniel Hjartland <squashical@gmail.com>
2023-05-06 21:22:47 +0200

    Complete implementation of clerk auth

--- src/env.mjs
+++ src/env.mjs
@@ -9,11 +9,19 @@ export const env = createEnv({
     },
     client: {
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().url(),
+        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().url(),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().url(),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().url(),
     },
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
         SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
         CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+        NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
     },
 });
