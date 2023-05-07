5fa35456f2ed4c3407d5da6c626fca36c404cbdc
Daniel Hjartland <squashical@gmail.com>
2023-05-07 09:49:59 +0200

    Add navbar
    Change styling of NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className="h-full">
         <body className={`${inter.className} h-full bg-zinc-900 text-zinc-200`}>
+          <NavBar />
           {children}
         </body>
       </html>
