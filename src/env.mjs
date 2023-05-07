d4060d7fa52ca0d7a5503c743fc2ae3f6a80bf74
Daniel Hjartland <squashical@gmail.com>
2023-05-07 09:51:12 +0200

    Add node env variable to enable development logging

--- src/env.mjs
+++ src/env.mjs
@@ -6,13 +6,14 @@ export const env = createEnv({
         DATABASE_URL: z.string().url(),
         SHADOW_DATABASE_URL: z.string().url(),
         CLERK_SECRET_KEY: z.string(),
+        NODE_ENV: z.string(),
     },
     client: {
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().url(),
+        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string(),
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string(),
     },
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
@@ -23,5 +24,6 @@ export const env = createEnv({
         NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
         NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
         NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+        NODE_ENV: process.env.NODE_ENV,
     },
 });
