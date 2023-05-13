b84cc54c89c91ca0e49d2caa6bb20781eb73d139
Daniel Hjartland <squashical@gmail.com>
2023-05-13 23:07:11 +0200

    Add basic loading UI

--- src/app/(app)/categories/page.tsx
+++ src/app/(app)/categories/page.tsx
@@ -27,7 +27,7 @@ export default async function CategoriesPage() {
 
   return (
     <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="sr-only">Products</h2>
+      <h2 className="sr-only">Categories</h2>
 
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {categories.map((category) => (
