c5e4e1d508ea3b2ed0ef033503d6e630b4aad62a
Daniel Hjartland <squashical@gmail.com>
2023-05-14 19:00:07 +0200

    Changed from using notFound to alerting users to no items being in that category

--- src/app/(app)/categories/[category]/page.tsx
+++ src/app/(app)/categories/[category]/page.tsx
@@ -1,5 +1,4 @@
 import { prisma } from "@/lib/db";
-import { notFound } from "next/navigation";
 import AddItemButton from "@/app/(app)/categories/[category]/AddItem";
 import { Metadata } from "next";
 import { toTitleCase } from "@/utils/formatString";
@@ -61,8 +60,26 @@ export default async function CategoryPage({ params: { category } }: Props) {
 
   const items = await getCategoryItems({ params: { category } });
   const sanitizedItems = sanitizeItems(items, userId);
+  console.log(sanitizedItems);
   if (items.length < 1 || !items) {
-    notFound();
+    return (
+      <>
+        <div className="flex w-full flex-col items-center justify-center gap-8 py-24">
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-5xl font-black tracking-tight">Sorry</h1>
+            <h2>No items found in that category</h2>
+          </div>
+          <p>
+            A feature to allow you to add items to this category is coming soon!
+          </p>
+        </div>
+        <h2 className="sr-only">Items</h2>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {/* @ts-expect-error */}
+          <AddItemButton segmentSlug={`${category}`} />
+        </div>
+      </>
+    );
   }
 
   return (
