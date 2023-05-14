48a6394258e3cb37f64589778ca407a46f1673d9
Daniel Hjartland <squashical@gmail.com>
2023-05-14 23:52:41 +0200

    Add a try catch block with error log

--- src/app/api/cron/stats/route.ts
+++ src/app/api/cron/stats/route.ts
@@ -5,19 +5,26 @@ import { getCategoryCount, getItemCount, getRatingCount } from "@/server/db";
 
 const redis = Redis.fromEnv();
 export async function GET() {
-  const userCount = await clerkClient.users.getCount();
-  const ratingCount = await getRatingCount();
-  const itemCount = await getItemCount();
-  const categoryCount = await getCategoryCount();
+  try {
+    const userCount = await clerkClient.users.getCount();
+    const ratingCount = await getRatingCount();
+    const itemCount = await getItemCount();
+    const categoryCount = await getCategoryCount();
 
-  const stats = [
-    { id: 1, name: "Registered users", value: userCount },
-    { id: 2, name: "Ratings made", value: ratingCount },
-    { id: 3, name: "Items ready to be rated", value: itemCount },
-    { id: 4, name: "Categories and counting", value: categoryCount },
-  ];
-  const response = await redis.set("stats", JSON.stringify(stats));
-  return new NextResponse(JSON.stringify(response), {
-    status: 200,
-  });
+    const stats = [
+      { id: 1, name: "Registered users", value: userCount },
+      { id: 2, name: "Ratings made", value: ratingCount },
+      { id: 3, name: "Items ready to be rated", value: itemCount },
+      { id: 4, name: "Categories and counting", value: categoryCount },
+    ];
+    const response = await redis.set("stats", JSON.stringify(stats));
+    return new NextResponse(JSON.stringify(response), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(JSON.stringify(error), {
+      status: 500,
+    });
+  }
 }
