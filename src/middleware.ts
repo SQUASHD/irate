c8d9d4a6f468a695a2965c6843ca7eabba9bbb01
Daniel Hjartland <squashical@gmail.com>
2023-05-14 19:18:23 +0200

    Change middleware config to run on app through /categories

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,8 @@ export default authMiddleware({
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+  matcher: [
+    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+    "/categories",
+  ],
 };
