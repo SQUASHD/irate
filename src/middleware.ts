c35aa75c2c6768a4ff66b011c4fb729bbd8e51c3
Daniel Hjartland <squashical@gmail.com>
2023-05-15 00:15:38 +0200

    Change middleware definition to use matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,13 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
   ignoredRoutes: [],
-  publicRoutes: ["/", "/about", "/api/cron"],
+  publicRoutes: ["/", "/about"],
 });
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)"],
+  matcher: [
+    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+    "/api/cron",
+  ],
 };
