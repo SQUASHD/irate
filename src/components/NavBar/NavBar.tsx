b3a9e6c1f7f6d86ecafa0760f525c6d677bdeea1
Daniel Hjartland <squashical@gmail.com>
2023-05-14 19:04:03 +0200

    Remove dev warning since it interfered with proper testing

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,4 @@
 import { IrateLogo } from "@/assets/icons";
-import DevWarning from "@/components/NavBar/DevelopmentAlert";
 import UserInfo from "@/components/NavBar/UserInfo";
 import { ClientSideDesktopNav } from "@/components/NavBar/ClientSideNav";
 import BreadCrumb from "@/components/NavBar/BreadCrumb";
@@ -7,8 +6,6 @@ import BreadCrumb from "@/components/NavBar/BreadCrumb";
 export default function NavBar() {
   return (
     <header className="absolute w-full">
-      {/* @ts-expect-error */}
-      <DevWarning />
       <nav className="w-full bg-gradient-to-b from-zinc-900">
         <div className="mx-auto flex max-w-2xl items-center justify-between px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="flex items-center gap-8">
