5c1c2026b847c0c555d3d13f239cb1d36fba4d53
Daniel Hjartland <squashical@gmail.com>
2023-05-16 20:21:29 +0200

    Revert middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: [
-    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
-    "/",
-    "/about",
-    "/api/cron(.*)",
-  ],
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)"],
 };
