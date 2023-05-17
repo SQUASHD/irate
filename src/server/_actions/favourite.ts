5a1dfd58683080194d188c06c106e9cf2d9bb7ea
Daniel Hjartland <squashical@gmail.com>
2023-05-17 13:46:06 +0200

    Create a server action to toggle favorites

---
+++ src/server/_actions/favourite.ts
@@ -0,0 +1,51 @@
+"use server";
+import { prisma } from "@/lib/db";
+import { z } from "zod";
+
+const favouriteSchema = z.object({
+  itemId: z.number(),
+  userId: z.string(),
+});
+
+export async function toggleFavorite(formData: FormData) {
+  const itemId = Number(formData.get("itemId"));
+  const userId = formData.get("userId");
+
+  const data = favouriteSchema.parse({
+    itemId: itemId,
+    userId: userId,
+  });
+
+  const favourite = await prisma.favourite.findFirst({
+    where: {
+      userId: data.userId,
+      itemId: data.itemId,
+    },
+  });
+
+  if (!favourite)
+    try {
+      return prisma.favourite.create({
+        data: {
+          userId: data.userId,
+          itemId: data.itemId,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  else {
+    try {
+      return prisma.favourite.update({
+        where: {
+          id: favourite.id,
+        },
+        data: {
+          favourited: !favourite.favourited,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
