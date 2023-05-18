2ed7a34d722508cfb8d11eff990aa201b14f0f0e
Daniel Hjartland <squashical@gmail.com>
2023-05-18 12:37:35 +0200

    Change add item button to have text as prop

--- src/app/categories/[category]/AddItem.tsx
+++ src/app/categories/[category]/AddItem.tsx
@@ -15,17 +15,21 @@ function PlusIcon(props: SVGProps) {
   );
 }
 
-interface AddItemButtonProps {
+type AddItemButtonProps = {
   segmentSlug: string;
-}
+  text: string;
+};
 
-export default function AddItemButton({ segmentSlug }: AddItemButtonProps) {
+export default function AddItemButton({
+  segmentSlug,
+  text,
+}: AddItemButtonProps) {
   return (
     <Link href={`/categories/${segmentSlug}/add`} className="group">
       <div className="group flex h-48 w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-zinc-200">
         <PlusIcon className="h-full p-8 text-zinc-400 group-hover:text-zinc-500" />
       </div>
-      <h3 className="mt-4 text-sm">Add new item</h3>
+      <h3 className="mt-4 text-sm">{text}</h3>
     </Link>
   );
 }
