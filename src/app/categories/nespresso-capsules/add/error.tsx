0a4af7237ab6f0d8fb0e0beea0923544e49ea51e
Daniel Hjartland <squashical@gmail.com>
2023-05-18 13:52:44 +0200

    Update nespresso error boundary

--- src/app/categories/nespresso-capsules/add/error.tsx
+++ src/app/categories/nespresso-capsules/add/error.tsx
@@ -15,16 +15,21 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="mx-auto h-full max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="flex h-full flex-col items-center justify-center">
+        <h2 className="text-center text-xl font-thin">Something went wrong</h2>
+        <h3 className="text-center text-4xl font-black">Oops!</h3>
+        <p className="py-4">Error: {error.message}</p>
+        <button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+          className="rounded-md bg-amber-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-amber-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500"
+        >
+          Try again
+        </button>
+      </div>
     </div>
   );
 }
