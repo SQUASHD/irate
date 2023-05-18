35d1c9be406d813165538712f3292a0159987da2
Daniel Hjartland <squashical@gmail.com>
2023-05-18 13:02:03 +0200

    Add business logic and checks to create item action

--- src/app/_actions/item.ts
+++ src/app/_actions/item.ts
@@ -10,42 +10,47 @@ export const create = async (formData: FormData) => {
   const { userId } = auth();
   if (!userId) throw new Error("User not logged in");
 
-  const data = createItemSchema.parse({
+  const data = {
     name: formData.get("name"),
     description: formData.get("description"),
     imageHref: formData.get("imageHref"),
     categorySlug: formData.get("categorySlug"),
-  });
+  };
+
+  const item = createItemSchema.safeParse(data);
+  if (!item.success) throw new Error("Invalid data");
+
   const [category] = await Promise.all([
     prisma.category.findFirst({
       where: {
-        slug: data.categorySlug,
+        slug: item.data.categorySlug,
       },
     }),
   ]);
+
   if (!category) throw new Error("Category does not exist");
 
   try {
-    const item = await prisma.item.create({
+    const newItem = await prisma.item.create({
       data: {
-        name: data.name,
-        description: data.description,
-        categoryId: data.categorySlug,
-        createdBy: data.userId,
+        name: item.data.name,
+        description: item.data.description,
+        categoryId: item.data.categorySlug,
+        createdBy: userId,
         images: {
           create: {
-            href: data.imageHref,
-            alt: data.name,
-            addedBy: data.userId,
+            href: item.data.imageHref,
+            alt: item.data.name,
+            addedBy: userId,
           },
         },
       },
     });
-    if (item) {
+    if (newItem) {
       revalidatePath(
-        `/categories/${data.categorySlug}/${encodeURI(item.name)}`
+        `/categories/${data.categorySlug}/${encodeURI(item.data.name)}`
       );
-      redirect(`/categories/${data.categorySlug}/${encodeURI(item.name)}`);
+      redirect(`/categories/${data.categorySlug}/${encodeURI(item.data.name)}`);
     }
   } catch (error) {
     throw new Error("Error creating item");
