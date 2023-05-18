2413894322ca70355dff6ae9dea0de8f7882c8aa
Daniel Hjartland <squashical@gmail.com>
2023-05-18 12:52:18 +0200

    Update category action to only need name, description and an image

--- src/app/_actions/category.ts
+++ src/app/_actions/category.ts
@@ -4,24 +4,19 @@ import { z } from "zod";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { createSlug } from "@/utils/stringUtils";
 
 export async function create(formData: FormData) {
   const { userId } = auth();
   if (!userId) throw new Error("User not logged in");
 
   const createCategorySchema = z.object({
-    slug: z
-      .string()
-      .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
-      .min(5)
-      .max(25),
-    name: z.string().max(50),
+    name: z.string().min(5).max(50),
     description: z.string().max(500).min(10),
     imageURL: z.string().url(),
   });
 
   const data = {
-    slug: formData.get("slug"),
     name: formData.get("name"),
     description: formData.get("description"),
     imageURL: formData.get("imageURL"),
@@ -30,10 +25,18 @@ export async function create(formData: FormData) {
   const category = createCategorySchema.safeParse(data);
   if (!category.success) throw new Error("Invalid data");
 
+  const categoryExists = await prisma.category.findUnique({
+    where: {
+      name: category.data.name,
+    },
+  });
+  if (categoryExists) throw new Error("Category already exists");
+  const generatedSlug = createSlug(category.data.name);
+
   try {
     await prisma.category.create({
       data: {
-        slug: category.data.slug,
+        slug: generatedSlug,
         name: category.data.name,
         description: category.data.description,
         createdBy: userId,
