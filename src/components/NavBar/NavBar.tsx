52d315fe6ade273c54a5611949cd0d3df14c5a02
Daniel Hjartland <squashical@gmail.com>
2023-05-18 12:39:50 +0200

    Add home link to logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { IrateLogo } from "@/assets/icons";
 import UserInfo from "@/components/NavBar/UserInfo";
 import { ClientSideDesktopNav } from "@/components/NavBar/ClientSideNav";
 import BreadCrumb from "@/components/NavBar/BreadCrumb";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
@@ -9,7 +10,10 @@ export default function NavBar() {
       <nav className="w-full bg-gradient-to-b from-zinc-900">
         <div className="mx-auto flex max-w-2xl items-center justify-between px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="flex items-center gap-8">
-            <IrateLogo className="h-6" />
+            <Link href={"/"}>
+              <IrateLogo className="h-6" />
+              <span className="sr-only">Home</span>
+            </Link>
             <div className="hidden gap-2 sm:flex">
               <ClientSideDesktopNav />
             </div>
