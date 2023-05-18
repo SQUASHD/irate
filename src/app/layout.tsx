c90a90b622843fd928d639d339c559aa479f7a21
Daniel Hjartland <squashical@gmail.com>
2023-05-18 16:08:29 +0200

    Change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en" className="h-full">
-        <body className={`${inter.className} h-full bg-zinc-950 text-zinc-200`}>
+        <body className={`${inter.className} h-full bg-zinc-900 text-zinc-200`}>
           {children}
         </body>
       </html>
