083ff22234ca65ae08977bac089e85ff3ee319cf
Daniel Hjartland <squashical@gmail.com>
2023-05-27 18:21:03 +0200

    Change styling to be unique from other apps I have made

--- components/ui/Links.tsx
+++ components/ui/Links.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 type StyledLinkProps = {
   href: string;
@@ -6,9 +7,14 @@ type StyledLinkProps = {
 };
 export default function StyledLink({ href, text }: StyledLinkProps) {
   return (
-    <Link href={href} className="group relative inline-block text-amber-400">
+    <Link
+      href={href}
+      className={cn(
+        "group relative inline-block text-amber-400",
+        "after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:max-w-full after:bg-amber-400 after:opacity-0 after:transition-all hover:after:bottom-0 hover:after:opacity-100"
+      )}
+    >
       {text}
-      <span className="relative bottom-2 block h-[1px] max-w-0 bg-current transition-all duration-500 group-hover:max-w-full"></span>
     </Link>
   );
 }
